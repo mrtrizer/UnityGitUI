99e835b1af9eca7d95f3c6fc6ffe3ac69fe27f7b
Zdorovtsov Ilya <ilyazdorovtsov@gmail.com>
2023-11-07 13:53:00 +0900

    Fix git log

--- Editor/GitLogWindow.cs
+++ Editor/GitLogWindow.cs
@@ -353,7 +353,7 @@ namespace Abuksigun.MRGitUI
             {
                 var contextMenuname = reference.QualifiedName.Replace("/", "\u2215");
                 string filesLableString = LogFiles != null && LogFiles.Any() ? "Files" : "";
-                string filesList = LogFiles.Join();
+                string filesList = LogFiles?.Join();
                 menu.AddItem(new GUIContent($"Checkout {filesLableString}/{contextMenuname}"), false, () => {
                     if (EditorUtility.DisplayDialog($"Are you sure you want CHECKOUT {filesLableString} to COMMIT", $"{reference.QualifiedName}\n{filesList}", "Yes", "No"))
                         _ = GUIUtils.RunGitAndErrorCheck(new[] { module }, x => x.Checkout(reference.QualifiedName, LogFiles));
